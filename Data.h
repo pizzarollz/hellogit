166846168870187e06d7ce2408ee30a91fe636d4
pizzarollz <daron.drayton@mycavehill.uwi.edu>
2018-10-10 15:20:36 -0400

    Added header file

---
+++ Data.h
@@ -0,0 +1,18 @@
+#ifndef DATA_H
+define DATA_H
+class Data
+{
+		private;
+					int x;
+					float y;
+					
+		public;
+					Data();
+					Data (int a, float b);
+					int getx();
+					void setx();
+					float getY();
+					void setY(float a);
+					
+};
+#endif
